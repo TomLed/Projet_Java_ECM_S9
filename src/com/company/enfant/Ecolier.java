01a475d514fc2968089faad473bb0b14000890ad
tledar <tomitom31@gmail.com>
2019-09-27 11:54:28 +0200

    Nouvelle version

--- src/com/company/enfant/Ecolier.java
+++ src/com/company/enfant/Ecolier.java
@@ -1,5 +1,5 @@
 package com.company.enfant;
 
 public class Ecolier extends Enfant {
-    
+    private static String niveauxClasses [] = {"CP", "CE1", "CE2", "CM1", "CM2"};
 }
--- src/com/company/enfant/Enfant.java
+++ src/com/company/enfant/Enfant.java
@@ -1,6 +1,56 @@
 package com.company.enfant;
 
 public class Enfant {
+    protected String Nom;
+    protected String Prenom;
+    protected int anneeNaissance;
+    protected char sexe;
+    protected int deptNaissance;
+    protected String Adresse;
 
+    public Enfant(String Nom, String Prenom, int anneeNaissance, char sexe, int deptNaissance, String Adresse){
+        this.Nom = Nom;
+        this.Prenom = Prenom;
+        this.anneeNaissance = anneeNaissance;
+        this.sexe = sexe;
+        this.deptNaissance = deptNaissance;
+        this.Adresse = Adresse;
+    }
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public void setPrenom(String prenom) {
+        Prenom = prenom;
+    }
+
+    public String getPrenom() {
+        return Prenom;
+    }
+
+    public int getAnneeNaissance() {
+        return anneeNaissance;
+    }
+
+    public void setSexe(char sexe) {
+        this.sexe = sexe;
+    }
+
+    public char getSexe() {
+        return sexe;
+    }
+
+    public int getDeptNaissance() {
+        return deptNaissance;
+    }
+
+    public void setAdresse(String adresse) {
+        Adresse = adresse;
+    }
+
+    public String getAdresse() {
+        return Adresse;
+    }
 
 }
